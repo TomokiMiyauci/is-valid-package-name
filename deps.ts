5c0d74f678568ec799b8fb25a06484d3e16b9e84
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-05-28 10:27:19 +0900

    :heavy_plus_sign:  Add dependency

---
+++ deps.ts
@@ -0,0 +1,39 @@
+// Copyright 2021-present the is-valid-package-name authors. All rights reserved. MIT license.
+export {
+  advance,
+  and,
+  entries,
+  first,
+  flattenDeep,
+  gt,
+  has,
+  ifElse,
+  isArray,
+  isBoolean,
+  isFunction,
+  isLength0,
+  isString,
+  isUndefined,
+  keys,
+  length,
+  lt,
+  map,
+  N,
+  NN,
+  not,
+  or,
+  pipe,
+  props,
+  startsWith,
+  trim,
+  tryCatch,
+  uniq,
+} from "https://deno.land/x/fonction@v1.8.0-beta.5/mod.ts";
+export {
+  failOnTrue,
+  gtLength,
+  isValid,
+  ltLength,
+} from "https://deno.land/x/is_valid@v1.0.0-beta.2/mod.ts";
+
+export type { AnyFn } from "https://deno.land/x/fonction@v1.8.0-beta.5/mod.ts";
---
+++ mod.ts
@@ -0,0 +1,2 @@
+// Copyright 2021-present the is-valid-package-name authors. All rights reserved. MIT license.
+export { isValid as isValidNestLand } from "./nest_land/mod.ts";
